2916536069a8c611d0d5a219ca23564012e9e09a
Макс <maxim__90.90@mail.ru>
2023-05-09 13:19:23 +0400

    first commit

---
+++ src/main/java/com/example/streams1/Streams1Application.java
@@ -0,0 +1,13 @@
+package com.example.streams1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streams1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Streams1Application.class, args);
+    }
+
+}
